5afc8b984a61020c4b6e16cc7e439f86c0e7ba33
Yuvraj Sakshith <37622719+yuvraj1803@users.noreply.github.com>
2022-07-20 16:38:49 +0530

    Template File Version 2

---
+++ t_v2.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#ifdef debug
+	#include "yuvraj/debug.h"
+#else
+	#define print(x) 18
+#endif
+
+#define int	long long
+using namespace std;
+
+signed main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+}
