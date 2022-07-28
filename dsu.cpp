c9e366ed4210b1aa6ca7e96ec0ef621e1f77d4b8
Yuvraj Sakshith <37622719+yuvraj1803@users.noreply.github.com>
2022-07-28 18:51:03 +0530

    Added noComps() and larComp()
    These two functions return the number of components in the graph and the size of the largest component in the graph respectively.

--- dsu.cpp
+++ dsu.cpp
@@ -3,14 +3,20 @@ public:
 	set<int> s;
 	vector<int> parent;
 	vector<int> rank;
+	set<int> comps;
+	int largestComp;
+	map<int,int> sz;
 	DSU(int n){
 		parent.resize(n);
 		rank.resize(n);
 		for(int i=0;i<n;i++){
-			s.ins(i);
+			s.insert(i);
 			parent[i] = i;
+			comps.insert(i);
+			sz[i] = 1;
 			rank[i] = 0;
 		}
+		largestComp = 1;
 	}
 
 	int Find(int x){
@@ -30,15 +36,36 @@ public:
 
 		if(rank[xR] < rank[yR]){
 			parent[xR] = yR;
+			if(comps.count(xR)) comps.erase(xR);
+			sz[yR]+=sz[xR];
+			sz[xR] = 0;
+			if(sz[yR] > largestComp) largestComp = sz[yR];
 		}else if(rank[xR] > rank[yR]){
 			parent[yR] = xR;
+			if(comps.count(yR)) comps.erase(yR);
+			sz[xR]+=sz[yR];
+			sz[yR] = 0;
+			if(sz[xR] > largestComp) largestComp = sz[xR];
 		}else{
 			parent[xR] = yR;
+			if(comps.count(xR)) comps.erase(xR);
 			rank[yR]++;
+			sz[yR]+=sz[xR];
+			sz[xR] = 0;
+			if(sz[yR] > largestComp) largestComp = sz[yR];
+
 		}
 	}
 
 	int Same(int x, int y){
 		return Find(x) == Find(y);
+	} 
+
+	int noComps(){ //returns the number of components
+		return (int)comps.size();
+	}
+	int larComp(){ //returns the largest component
+		return largestComp;	
 	}
 };
+
